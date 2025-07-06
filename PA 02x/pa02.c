09c56e568af6393d82b134da865452944f8a46b4
SlyeJeremiah <155405463+SlyeJeremiah@users.noreply.github.com>
2025-07-06 16:48:55 -0400

    Update pa02.c

--- PA 02x/pa02.c
+++ PA 02x/pa02.c
@@ -80,14 +80,88 @@ int main(int argc, char **argv) {
         printf("\n");
     }
 
+    // open the plaintext file
+    FILE *pfp = fopen(plainFile, "r");
+    // make sure there was no error when the file was opened
+    if (pfp == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
 
-    
-    
-    
-    
-    
-    
-    
+    // buffer to store the characters (assigment said 10k is cool)
+    char plainBuffer[10000];
+    // keep track of the num of characters
+    int plainLength = 0;
+    int character;
+
+    while ((character = fgetc(plainFile)) != EOF) {
+        // test to see if the character is uppercase and if it is
+        // make it lowercase
+        if (character >= 'A' && character <= 'Z') {
+            character = character + ('a' - 'A'); 
+            // ascii trick to add 32 to to the character making it lowercase (the upper and lower are 32 apart)
+        }
+
+        // test if the character is lowercase and if it is add it to the buffer
+        if (character >= 'a' && character <= 'z') {
+            plainBuffer[plainLength++] = character;
+        }
+    }
+
+    // now i have to pad if the length of the buffer isnt a multiple of n
+    int leftOver = plainLength % n;
+    if (leftOver != 0) {
+        // its easier to find how much to pad instead of looping
+        // the padding and then testing to see if its a multiple of n
+        int padCount = n - leftOver; // the leftover will always be less than n bc of the math (never negative)
+        for (int i = 0; i < padCount; i++) {
+            plainBuffer[plainLength++] = 'x';
+        }
+    }
+
+    // echo the plain text
+    printf("\nPlaintext:\n");
+    for (int i = 0; i < plainLength; i++) {
+        putchar(plainBuffer[i]);
+        if ((i+1) % 80 == 0) { // after every 80 characters (when i = 79, 159, ...) print a newline
+            putchar('\n');
+        }
+    }
+    // only add a final newline if the last character wasnt already a newline
+    if (plainLength % 80 != 0) {
+        putchar('\n');
+    }
+
+    // another trick to map a to 0, b to 1, etc is just subtracting whatever letter by a
+    // and to get a letter back is to add a (a is 97 in ascii)
+    char cipherBuffer[10000];
+    for (int i = 0; i < plainLength; i = i + n) {
+        // jumping by n gives the right block sizes
+        // martix mult: i have to multiply the rows of the key matrix
+        // by the cols of the plaintext matrix
+        for (int row = 0; row < n; row++) {
+            int sum = 0;
+            for (int col = 0; col < n; col++) {
+                int plainValue = plainBuffer[i + col] - 'a'; // chages the letter to a number
+                sum = sum + keyMatrix[row][col] * plainValue;
+            }
+           cipherBuffer[i + row] = (sum % 26) + 'a'; // change the num to a letter 
+        }
+    }
+
+    // echo the cipher text
+    printf("\nCiphertext:\n");
+    for (int i = 0; i < plainLength; i++) {
+        putchar(cipherBuffer[i]);
+        if ((i+1) % 80 == 0) { // after every 80 characters (when i = 79, 159, ...) print a newline
+            putchar('\n');
+        }
+    }
+    // only add a final newline if the last character wasnt already a newline
+    if (plainLength % 80 != 0) {
+        putchar('\n');
+    }
+        
     return 0;
 }
 
